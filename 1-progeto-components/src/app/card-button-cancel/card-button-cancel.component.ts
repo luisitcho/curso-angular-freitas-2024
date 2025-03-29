7613b7b56ebefaf8138da68988a93a2a22c1bc3a
luisitcho <luishenrisc1@gmail.com>
2025-03-29 15:25:36 -0300

    curso angular - template

---
+++ 1-progeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-card-button-cancel',
+    standalone: false,
+    templateUrl: './card-button-cancel.component.html',
+    //   styleUrl: './card-button-cancel.component.scss'
+    styles: `
+        .card-cancel-button {
+        font-size: 16px;
+        text-align: center;
+        margin-top: 10px;
+        border-radius: 25px;
+        background-color: red;
+        cursor: pointer;
+        font-weight: bold;
+        }
+    `,
+})
+export class CardButtonCancelComponent {
+
+}
