27e5846119d4dd1581b4e80f93b717ad3022813c
luisitcho <luishenrisc1@gmail.com>
2025-03-29 12:06:20 -0300

    curso angular - interpolação

--- 1-progeto-components/src/app/card/card.component.ts
+++ 1-progeto-components/src/app/card/card.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  standalone: false,
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+    selector: 'app-card',
+    standalone: false,
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.scss'
 })
 export class CardComponent {
+    tipo: string = 'Simples';
+    preco: string = (100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
+    getFullPrice() {
+
+        return `${this.preco}/Mês`;
+    }
+
+    plano = {
+        tipo: 'Simples',
+        preco: (100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+        getFullPrice() {
+
+            return `${this.preco}/Mês`;
+        }
+    }
 }
