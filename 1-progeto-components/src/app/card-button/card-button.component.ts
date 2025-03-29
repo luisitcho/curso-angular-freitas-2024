33944e22973db949f083aab6f2236b153067c796
luisitcho <luishenrisc1@gmail.com>
2025-03-29 12:37:08 -0300

    curso angular - template

--- 1-progeto-components/src/app/card-button/card-button.component.ts
+++ 1-progeto-components/src/app/card-button/card-button.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card-button',
-  standalone: false,
-  templateUrl: './card-button.component.html',
-  styleUrl: './card-button.component.scss'
+    selector: 'app-card-button',
+    standalone: false,
+    //   templateUrl: './card-button.component.html',
+    template: `
+        <div class="card-button cursor-pointer p-2">Adquirir</div>
+    `,
+    styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
 
