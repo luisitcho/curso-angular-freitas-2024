76a229401d39a65832fe9bd7822be5cf37868347
luisitcho <luishenrisc1@gmail.com>
2025-03-30 17:13:07 -0300

    curso angular - ViewEncapsulation.None

--- 1-progeto-components/src/app/card/card.component.ts
+++ 1-progeto-components/src/app/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-card',
     standalone: false,
     templateUrl: './card.component.html',
-    styleUrl: './card.component.scss'
+    styleUrl: './card.component.scss',
+    encapsulation: ViewEncapsulation.None
 })
 export class CardComponent {
     tipo: string = 'Simples';
