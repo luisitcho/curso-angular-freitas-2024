8c45b90fef96faae1b66f901d67e49ad48134620
luisitcho <luishenrisc1@gmail.com>
2025-04-10 23:07:07 -0300

    curso angular - Property Binding

--- 2-projeto-eventos/src/app/app.component.ts
+++ 2-projeto-eventos/src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '2-projeto-eventos';
+    title = '2-projeto-eventos';
+    inputText = 'texto inicial';
+    inputType = 'text';
+    isDisabled = true;
+
+    enableInput() {
+        this.isDisabled = false;
+    }
+
+    disableInput() {
+        this.isDisabled = true;
+    }
+
+    setPasswornTypeInput() {
+        this.inputType = 'password';
+    }
+
+    setTextTypeInput() {
+        this.inputType = 'text';
+    }
+
+    logInputText() {
+        console.log(this.inputText);
+    }
 }
