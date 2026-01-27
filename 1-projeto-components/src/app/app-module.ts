9f45a6c2b69bd172c7455699c836ed9527b594ac
luisitcho <luishenrisc1@gmail.com>
2026-01-27 00:07:46 -0300

    curso angular - Criação de Componentes Filhos

--- 1-projeto-components/src/app/app-module.ts
+++ 1-projeto-components/src/app/app-module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Card } from './card/card';
 import { CardRoxo } from './card-roxo/card-roxo';
+import { CardButton } from './card-button/card-button';
+import { CardRoxoButton } from './card-roxo-button/card-roxo-button';
 
 @NgModule({
   declarations: [
     App,
     Card,
-    CardRoxo
+    CardRoxo,
+    CardButton,
+    CardRoxoButton
   ],
   imports: [
     BrowserModule,
