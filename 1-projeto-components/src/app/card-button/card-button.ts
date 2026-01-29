e51325d998ad608d2acff517dfd0feaa09b5c0c5
luisitcho <luishenrisc1@gmail.com>
2026-01-28 23:58:32 -0300

    curso angular - Template Dentro do Componente (prop template)

--- 1-projeto-components/src/app/card-button/card-button.ts
+++ 1-projeto-components/src/app/card-button/card-button.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card-button',
-  standalone: false,
-  templateUrl: './card-button.html',
-  styleUrl: './card-button.scss',
-})
-export class CardButton {
+    selector: 'app-card-button',
+    standalone: false,
+    //templateUrl: './card-button.html',
+    template: ` <div class="card-button">Teste Template</div> `,
 
-}
+    styleUrl: './card-button.scss',
+})
+export class CardButton {}
