f81971661809a3bb3b5872c86afe0ce27853828d
luisitcho <luishenrisc1@gmail.com>
2026-01-28 22:42:01 -0300

    curso angular - Utilização da Interpolação

--- 1-projeto-components/src/app/card/card.ts
+++ 1-projeto-components/src/app/card/card.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  standalone: false,
-  templateUrl: './card.html',
-  styleUrl: './card.scss',
+    selector: 'app-card',
+    standalone: false,
+    templateUrl: './card.html',
+    styleUrl: './card.scss',
 })
 export class Card {
-
+    plano = {
+        tipo: 'Simples',
+        preco: Number(100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+        get_preco(): string {
+            return `${this.preco}/Mês`;
+        },
+    };
 }
